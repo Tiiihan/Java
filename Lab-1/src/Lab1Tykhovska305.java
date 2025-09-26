b971281780b9c7bab5c4afaa5e62f1566ea565bd
Yuliia Tykhovska <uliatihovska1@gmail.com>
2025-09-26 09:41:05 +0300

    Added Lab-1

--- Lab-1/src/Lab1Tykhovska305.java
+++ Lab-1/src/Lab1Tykhovska305.java
@@ -3,25 +3,28 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 /**
  * Лабораторна робота №1.
- * Програма створює рівнобедрений трикутник, який починається із центру квадрата.
- * Символ для створення трикутника, а також розмір квадратної матриці задає користувач.
- * Створений масив виводиться в консоль та записується у текстовий файл.
+ * Програма будує рівнобедрений трикутник, що починається з центру квадратної матриці.
+ * Користувач задає розмір квадратної матриці , символ для побудови трикутника.
+ * Результат виводиться у консоль та зберігається у текстовий файл.
  */
 public class Lab1Tykhovska305 {
 
     /**
-     * Статичний метод main є точкою входу в програму
+     * Точка входу у програму.
+     *
      * @param args
+     * @throws FileNotFoundException якщо не вдається створити або відкрити файл.
      */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner in = new Scanner(System.in);
 
-        // Отримання розміру матриці від користувача
+        // Файл для збереження результату
+        File dataFile = new File("MyFile.txt");
+        PrintWriter fout = new PrintWriter(dataFile);
+
+        // Отримання розміру квадратної матриці
         System.out.print("Enter size of square matrix: ");
         int nRow = in.nextInt();
         in.nextLine();
@@ -30,72 +33,59 @@ public class Lab1Tykhovska305 {
         System.out.print("Enter symbol to draw triangle: ");
         String symbol = in.nextLine();
 
-        // Перевірка коректності введеного символу
-        while(symbol.length() > 1 || symbol.length() < 1)
-        {
+        // Перевірка правильності введення символу
+        while (symbol.length() != 1) {
             System.out.print("Enter correct symbol to draw triangle: ");
             symbol = in.nextLine();
         }
 
-        // Визначення центру трикутника
+        // Центр квадратної матриці
         int center = nRow / 2;
 
-        // Зубчастий масив для зберігання трикутника
+        // Зубчастий масив
         char[][] arr = new char[nRow][];
 
-        // Запис результату у файл з обробкою винятків
-        try (PrintWriter fout = new PrintWriter(new File("MyFile.txt"))) {
+        // Формування зубчастого масиву
+        for (int i = 0; i < nRow; i++) {
+            if (i >= center) {
+                int level = i - center; // відстань від центру
+                int lengthOfArr = 2 * level + 1; // кількість символів у рядку
+                arr[i] = new char[lengthOfArr];
 
-            /**
-             * Малюємо трикутник і зберігаємо у зубчастий масив.
-             * Трикутник будується від центру донизу.
-             */
-            for (int i = 0; i < nRow; i++) {
-                if (i < center) {
-                    // Порожні рядки зверху (до центру)
-                    arr[i] = new char[0];  // рядок без символів
-                    System.out.println();
-                    fout.println();
-                }
-                else {
-                    /**
-                     * Обчислення параметрів для поточного рядка трикутника:
-                     * - currentLevel: рівень трикутника (відстань від центру)
-                     * - leftBound: ліва межа заповнення символами
-                     * - rightBound: права межа заповнення символами
-                     * - length: кількість символів у поточному рядку
-                     */
-                    int currentLevel = i - center;
-                    int leftBound = center - currentLevel;
-                    int rightBound = center + currentLevel;
-                    int length = rightBound - leftBound + 1;
-
-                    // Ініціалізація масиву для поточного рядка
-                    arr[i] = new char[length];
-
-                    // Заповнення масиву символами
-                    for (int j = 0; j < length; j++) {
-                        arr[i][j] = (char) symbol.codePointAt(0);
-                    }
-
-                    // Вивід на консоль
-                    for (int j = 0; j < leftBound; j++) System.out.print(" ");
-                    for (char c : arr[i]) System.out.print(c);
-                    System.out.println();
-
-                    // Вивід у файл
-                    for (int j = 0; j < leftBound; j++) fout.print(" ");
-                    for (char c : arr[i]) fout.print(c);
-                    fout.println();
+                // Заповнення рядка
+                for (int j = 0; j < lengthOfArr; j++) {
+                    arr[i][j] = (char) symbol.codePointAt(0);
                 }
             }
         }
-        catch (FileNotFoundException e) {
-            /**
-             * Обробка помилки створення файлу
-             * @exception FileNotFoundException
-             */
-            System.out.println("Error creating file: " + e.getMessage());
+
+        // Виведення трикутника в консоль і файл
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null && arr[i].length > 0) {
+                int level = i - center;
+                int numOfSpaces = center - level; // кількість пробілів для центрування
+
+                // Відступи для правильного відображення
+                for (int s = 0; s < numOfSpaces; s++) {
+                    System.out.print(" ");
+                    fout.print(" ");
+                }
+
+                // Виведення символів трикутника
+                for (int j = 0; j < arr[i].length; j++) {
+                    System.out.print(arr[i][j]);
+                    fout.print(arr[i][j]);
+                }
+
+                System.out.println();
+                fout.println();
+            } else {
+                System.out.println();
+                fout.println();
+            }
         }
+
+        fout.flush();
+        fout.close();
     }
 }
--- Lab-1/src/docs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"Lab1Tykhovska305","l":"Lab1Tykhovska305()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Lab1Tykhovska305","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
